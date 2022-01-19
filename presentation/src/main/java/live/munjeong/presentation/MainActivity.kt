8c887718606fe23fc29a5487291eba1149c7a25a
sangcomz <dev.seokwon2@gmail.com>
2022-01-20 07:25:10 +0900

    add splash

--- presentation/src/main/java/live/munjeong/presentation/MainActivity.kt
+++ presentation/src/main/java/live/munjeong/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package live.munjeong.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import live.munjeong.presentation.navigation.MunJeongNavGraph
 
@@ -10,6 +11,30 @@ import live.munjeong.presentation.navigation.MunJeongNavGraph
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashWasDisplayed = savedInstanceState != null
+        if (!splashWasDisplayed) {
+            startSplash()
+        } else {
+            setContent()
+        }
+    }
+
+    private fun startSplash() {
+        val splashScreen = installSplashScreen()
+
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            splashScreenViewProvider.iconView
+                .animate()
+                .setDuration(300)
+                .alpha(0f)
+                .withEndAction {
+                    splashScreenViewProvider.remove()
+                    setContent()
+                }.start()
+        }
+    }
+
+    private fun setContent() {
         setContent {
             MunJeongApp()
         }
